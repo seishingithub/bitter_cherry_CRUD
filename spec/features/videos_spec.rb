996b80b2728247c8da71afb7d75b4c23d3a2943b
Peggy Griffin <seishingithub@gmail.com>
2014-04-28 16:24:35 -0600

    [#70291370] User can see a new video form. Not yet styled

--- spec/features/videos_spec.rb
+++ spec/features/videos_spec.rb
@@ -8,4 +8,13 @@ feature 'User can manage videos' do
     expect(page).to have_content('Videos')
   end
 
+  scenario 'user should be able to see a new video form' do
+    visit '/'
+    click_on 'all videos'
+    click_on 'New Video'
+    expect(page).to have_content('URL')
+    expect(page).to have_content('Description')
+    expect(page).to have_content('Rating')
+  end
+
 end
