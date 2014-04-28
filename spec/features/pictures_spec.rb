5184c91081bcba9ba2d2f48c2615b371582edc6f
Peggy Griffin <seishingithub@gmail.com>
2014-04-28 16:50:55 -0600

    Changed pictures form to match comps

--- spec/features/pictures_spec.rb
+++ spec/features/pictures_spec.rb
@@ -6,8 +6,8 @@ feature "Manage Pictures" do
     click_on 'all pictures'
     click_on 'New Picture'
     expect(page).to have_content "Add a Picture"
-    fill_in 'Url', with: 'http://listdose.com/wp-content/uploads/2013/07/coffee-ending.jpg'
-    fill_in 'Description', with: 'Life is short.'
+    fill_in 'URL of Video', with: 'http://listdose.com/wp-content/uploads/2013/07/coffee-ending.jpg'
+    fill_in 'Short Description', with: 'Life is short.'
     fill_in 'Rating', with: 5
   end
 end
