fa862ed9bb7ef88b5ecba8cd06e7ab602545325c
Zach Klabunde <zbunde@gmail.com>
2014-04-28 16:56:57 -0600

    user can add/view/index vimeo videos

--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20140428204857) do
-=======
 ActiveRecord::Schema.define(version: 20140428214619) do
->>>>>>> fd6ac9d90d2ea733955d452cc3d0bd5f18563681
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-  create_table "vimeo_videos", force: true do |t|
-    t.string  "url"
-    t.string  "description"
-    t.integer "rating"
-=======
   create_table "pictures", force: true do |t|
     t.string   "url",         null: false
     t.string   "description"
     t.integer  "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
->>>>>>> fd6ac9d90d2ea733955d452cc3d0bd5f18563681
+  end
+
+  create_table "vimeo_videos", force: true do |t|
+    t.string  "url"
+    t.string  "description"
+    t.integer "rating"
   end
 
 end
