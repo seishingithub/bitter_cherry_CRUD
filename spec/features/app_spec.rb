e80763dd2441dbfdfd0ae06b5a532ce23dc629a2
BlueVajra <cory.leistikow@gmail.com>
2014-04-28 13:52:46 -0600

    Added styles for Homepage header and footer

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -4,5 +4,8 @@ feature "Manage Media" do
   scenario "User sees Media Manager onthe home screen" do
     visit '/'
     expect(page).to have_content 'Media Manager'
+    expect(page).to have_content 'all pictures'
+    expect(page).to have_content 'all videos'
+    expect(page).to have_content 'Not copyrighted at all'
   end
 end
