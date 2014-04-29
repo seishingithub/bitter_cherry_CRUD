7d209ac4e646bb7dc92bcae51a7df0f857660dea
Cory Leistikow <cory.leistikow@gmail.com>
2014-04-29 14:42:17 -0600

    fixed pictures show page layout
    Signed-off-by: Gerard Cote <grcote@gmail.com>

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,13 +8,11 @@ class PicturesController < ApplicationController
   end
 
   def create
-    p params[:picture][:description]
     @picture = Picture.new
     @picture.url = params[:picture][:url]
     @picture.description = params[:picture][:description]
     @picture.rating = params[:picture][:rating]
     @picture.save
-
 
     redirect_to picture_path(@picture), notice: "Picture successfully created"
   end
