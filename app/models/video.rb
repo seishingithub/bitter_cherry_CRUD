3c464e24b121fba616b4edb1140d587ac474a353
Cory Leistikow <cory.leistikow@gmail.com>
2014-04-30 16:50:32 -0600

    [#70482814] User no longer gets an error when entering an 'https' valid youtube url

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ActiveRecord::Base
   validates :url, :description, :presence => true
 
-  validates :url, format: { with: /http:\/\/www.youtube.com\/watch\?v=\w*/i, message: 'must be a valid YouTube url' }
+  validates :url, format: { with: /(http|https):\/\/www.youtube.com\/watch\?v=\w*/i, message: 'must be a valid YouTube url' }
 end
