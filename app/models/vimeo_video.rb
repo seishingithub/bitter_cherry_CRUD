a597ca68281d5aedf27b31ea57bab168c5f932d3
Zach Klabunde <zbunde@gmail.com>
2014-05-01 11:29:36 -0600

    added validation to make sure submitted URl is a valid vimeo link

--- app/models/vimeo_video.rb
+++ app/models/vimeo_video.rb
@@ -1,3 +1,5 @@
 class VimeoVideo < ActiveRecord::Base
   validates :url, :description, :rating, :presence => true
+  validates :url, format: { with: /(http|https):\/\/vimeo.com\/\d*/, message: 'must be a valid vimeo url' }
+
 end
