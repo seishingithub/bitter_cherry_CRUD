8e8267b2bca14973ba0c35c8f021539297b98904
Gerard Cote <grcote@gmail.com>
2014-05-01 11:56:20 -0600

    [#70525410]User cannot rank a video higher than 5 or lower than 1
    Signed-off-by: Emily Platzer <emilyplatzer@gmail.com>

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,7 @@ class Video < ActiveRecord::Base
   validates :url, :description, :presence => true
 
   validates :url, format: { with: /(http|https):\/\/www.youtube.com\/watch\?v=\w*/i, message: 'must be a valid YouTube url' }
+
+  validates :rating, inclusion: { within: [1,2,3,4,5], message: "%{value} is not valid."}
+
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Video do
   describe 'validations' do
     before do
-      @video = Video.new(:url => 'http://www.youtube.com/watch?v=QvxdDDHElZo', :description => 'Oh Hai!')
+      @video = Video.new(:url => 'http://www.youtube.com/watch?v=QvxdDDHElZo', :description => 'Oh Hai!', :rating => 3)
       expect(@video).to be_valid
     end
 
@@ -21,5 +21,16 @@ describe Video do
       @video.description = ''
       expect(@video).to_not be_valid
     end
+
+    it 'ensures rating cannot be greater than 5' do
+      @video.rating = 6
+      expect(@video).to_not be_valid
+    end
+
+    it 'ensures rating cannot be less than 1' do
+      @video.rating = 0
+      expect(@video).to_not be_valid
+    end
+
   end
 end
